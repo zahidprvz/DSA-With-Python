64f7da061f2053d34b4a7a0e686143997bccfc3f
Zahid Parviz <pervaizzahid55@gmail.com>
2025-01-16 10:01:02 +0500

    written code for DoubleLinkedList and basic and intermediate related methods to work with a DLL

--- LL/DoublyLinkedList.py
+++ LL/DoublyLinkedList.py
@@ -12,50 +12,157 @@ class DoublyLinkedList:
         self.length = 1
 
     def print_list(self):
-        """Prints all values in the linked list."""
-        if self.length == 0:
-            print("There are no elements in LL")
-            return
         temp = self.head
-        while temp:
+        while(temp):
             print(temp.value)
             temp = temp.next
 
-    # ==========================
-    # Core Operations
-    # ==========================
     def append(self, value):
-        """Adds a node with the given value to the end of the linked list."""
         new_node = Node(value)
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
+            new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
         return True
 
     def pop(self):
-        """Removes and returns the last node from the doubly linked list."""
-        if self.head is None:  # Case: Empty list
+        if self.head is None:
             return None
-
-        temp = self.head
+        temp = self.tail
         if self.length == 1:
             self.head = None
             self.tail = None
         else:
             self.tail = self.tail.prev
             self.tail.next = None
-            self.tail = None
+            temp.prev = None
         self.length -= 1
         return temp
 
-my_d_ll = DoublyLinkedList(1)
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+
+        temp = self.get(index)
+
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+
+        self.length -= 1
+        return temp
+
+    def swap_first_and_last(self):
+        if self.head is None or self.head == self.tail:
+            return None
+        
+
+
+my_d_ll = DoublyLinkedList(0)
+my_d_ll.append(1)
 my_d_ll.append(2)
-my_d_ll.append(3)
-my_d_ll.append(4)
-my_d_ll.append(5)
+my_d_ll.append(99)
+
+print("Initial DLL: ")
 my_d_ll.print_list()
-print(my_d_ll.pop())
+
+print("\n\nDLL after poping the node: ")
+my_d_ll.pop()
+my_d_ll.print_list()
+
+print("\n\nDLL after prepending a node: ")
+my_d_ll.prepend(-1)
+my_d_ll.print_list()
+
+print("\n\nAfter popping_first element: ")
+my_d_ll.pop_first()
+my_d_ll.print_list()
+
+print("\n\nThe element at index 2 is: ", my_d_ll.get(2))
+
+print("\n\nAfter changing the element at index 2: ")
+my_d_ll.set_value(2, 143)
+my_d_ll.print_list()
+
+print("\n\nAfter inserting new node at index 1: ")
+my_d_ll.insert(1, 108)
+my_d_ll.print_list()
+
+print("\n\nAfter removing the node at index 0: ")
+my_d_ll.remove(0)
+my_d_ll.print_list()
